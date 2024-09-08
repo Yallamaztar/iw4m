218fb63b52f81d63ca2e5f731e0f3794f261f080
Yalla <132653472+Yallamaztar@users.noreply.github.com>
2024-09-08 10:47:21 +0300

    Add asynchronous support to IW4MWrapper
    Brownies on top

--- iw4m/IW4MWrapper.py
+++ iw4m/IW4MWrapper.py
@@ -1,25 +1,70 @@
 
+import aiohttp, asyncio, aiofiles
 from requests import sessions
+from datetime import datetime
+import os, tempfile
+import logging
+
+LOGFILE = os.path.join(tempfile.gettempdir(), '{0}.{1}.log'.format(
+    os.path.basename(__file__),
+    datetime.now().strftime('%Y%m%dT%H%M%S')
+))
+
+logging.basicConfig(
+    filename=LOGFILE,
+    level=logging.DEBUG,
+    format='%(asctime)s - %(levelname)s - %(message)s',
+)
 
 class IW4MWrapper():
-    def __init__(self, server_address: str, server_id: int, cookie: str):
-        self.server_address = server_address
+    def __init__(self, base_url: str, server_id: int, cookie: str, _logging: bool):
+        self.base_url = base_url
         self.server_id = server_id
         self.session = sessions.Session()
         self.session.headers.update({ "Cookie": cookie })
+        self._logging = _logging
 
+        if self._logging:
+            logging.info(f"Initialized IW4MWrapper for server {self.base_url} with ID {self.server_id}")
 
     def send_command(self, command):
-        return self.session.get(f"{self.server_address}/Console/Execute?serverId={self.server_id}&command={command}")
+        if self._logging:
+            logging.info(f"Sending command '{command}' to server {self.base_url} with ID {self.server_id}")
+            
+        try:
+            response = self.session.get(f"{self.base_url}/Console/Execute?serverId={self.server_id}&command={command}")
+            if self._logging:
+                logging.info(f"Command '{command}' executed successfully with response status: {response.status_code}")
+                
+            return response.text
+        
+        except Exception as e:
+            if self._logging:
+                logging.error(f"Error executing command '{command}': {e}")
+            raise
+
+    def get_logs(self):
+        temp_dir = tempfile.gettempdir()
+        logs = [file for file in os.listdir(temp_dir) if file.endswith(".log")]
+
+        if not logs:
+            logging.exception("No log files found")
+
+        files = sorted(logs, key=lambda file: os.path.getctime(os.path.join(temp_dir, file)), reverse=True)
+        path = os.path.join(temp_dir, files[0])
+
+        try:
+            with open(path, 'r') as f:
+                return f.read()
+        except Exception as e:
+            logging.error(f"Error reading log file: {e}")  
+            return f"Error reading log file: {e}"   
 
     class Commands:
         def __init__(self, wrapper):
             self.wrapper = wrapper
 
-        #-----------------#
         #  Command List   #
-        #-----------------#
-
         def setlevel(self, player, level):
             return self.wrapper.send_command(f"!setlevel {player} {level}")
 
@@ -130,11 +175,8 @@ class IW4MWrapper():
         
         def report(self, player, reason): 
             return self.wrapper.send_command(f"!report {player} {reason}")
-        
-        #-----------------#
-        #  Script Plugin  #
-        #-----------------#
 
+        #  Script Plugin  #
         def giveweapon(self, player, weapon):
             return self.wrapper.send_command(f"!giveweapon {player} {weapon}")
         
@@ -179,18 +221,12 @@ class IW4MWrapper():
         
         def switchteam(self, player):
             return self.wrapper.send_command(f"!switchteam {player}")
-        
-        #-----------------#
-        #      Login      #
-        #-----------------#
 
+        #      Login      #
         def login(self, password):
             return self.wrapper.send_command(f"!login {password}")
-        
-        #-----------------#
-        #       Mute      #
-        #-----------------#
 
+        #       Mute      #
         def mute(self, player, reason):
             return self.wrapper.send_command(f"!mute {player} {reason}")
         
@@ -203,10 +239,7 @@ class IW4MWrapper():
         def unmute(self, player, reason):
             return self.wrapper.send_command(f"!unmute {player} {reason}")
         
-        #-----------------#
         #  Simple Status  #
-        #-----------------#
-
         def mostkills(self):
             return self.wrapper.send_command("!mostkills")
         
@@ -225,9 +258,247 @@ class IW4MWrapper():
             else:
                 return self.wrapper.send_command(f"!x {player}")
 
-iw4m = IW4MWrapper(
-    server_address="http://your.server.com", 
-    server_id=123456789,
-    cookie="your-aspnet-cookie"
-)
+class AsyncIW4MWrapper():
+    def __init__(self, base_url: str, server_id: int, cookie: str, _logging: bool):
+        self.base_url = base_url
+        self.server_id = server_id
+        self.cookie = cookie
+        self._logging = _logging
 
+        if self._logging:
+            logging.info(f"Initialized IW4MWrapper for server {self.base_url} with ID {self.server_id}")
+
+    async def send_command(self, command):
+        if self._logging:
+            logging.info(f"Sending command '{command}' to server {self.base_url} with ID {self.server_id}")
+
+        async with aiohttp.ClientSession() as session:
+            try:
+                async with session.get(
+                    f"{self.base_url}/Console/Execute?serverId={self.server_id}&command={command}",
+                    headers={"Cookie": self.cookie}
+                ) as response:
+                    res = await response.text()
+                    logging.info(f"Command '{command}' executed successfully with response status: {response.status}")
+                return res
+            
+            except aiohttp.ClientError as e:
+                if self._logging:
+                    logging.error(f"Error executing command '{command}': {e}")
+                raise
+    class Commands:
+        def __init__(self, wrapper):
+            self.wrapper = wrapper
+
+        #  Command List   #
+        async def setlevel(self, player, level):
+            return await self.wrapper.send_command(f"!setlevel {player} {level}")
+
+        async def change_map(self, map_name):
+            return await self.wrapper.send_command(f"!map {map_name}")
+    
+        async def ban(self, player, reason):
+            return await self.wrapper.send_command(f"!ban {player} {reason}")
+        
+        async def unban(self, player, reason):
+            return await self.wrapper.send_command(f"!unban {player} {reason}")
+
+        async def fastrestart(self):
+            return await self.wrapper.send_command("!fastrestart")
+
+        async def maprotate(self):
+            return await self.wrapper.send_command("!mr")
+    
+        async def requesttoken(self):
+            return await self.wrapper.send_command("!requesttoken")
+    
+        async def clearallreports(self):
+            return await self.wrapper.send_command("!clearallreports")
+    
+        async def alias(self, player):
+            return await self.wrapper.send_command(f"!alias {player}")
+
+        async def whoami(self):
+            return await self.wrapper.send_command("!whoami")
+    
+        async def warn(self, player, reason):
+            return await self.wrapper.send_command(f"!warn {player} {reason}")
+
+        async def warnclear(self, player):
+            return await self.wrapper.send_command(f"!warnclear {player}")
+
+        async def kick(self, player):
+            return await self.wrapper.send_command(f"!kick {player}")
+
+        async def tempban(self, player, duration, reason):
+            return await self.wrapper.send_command(f"!tempban {player} {duration} {reason}")
+
+        async def usage(self):
+            return await self.wrapper.send_command("!usage")
+    
+        async def uptime(self):
+            return await self.wrapper.send_command("!uptime")
+    
+        async def flag(self, player, reason):
+            return await self.wrapper.send_command(f"!flag {player} {reason}")
+    
+        async def unflag(self, player, reason):
+            return await self.wrapper.send_command(f"!unflag {player} {reason}")
+
+        async def mask(self):
+            return await self.wrapper.send_command("!mask")
+    
+        async def baninfo(self, player):
+            return await self.wrapper.send_command(f"!baninfo {player}")
+
+        async def setpassword(self, password):
+            return await self.wrapper.send_command(f"!setpassword {password}")
+
+        async def runas(self, command):
+            return await self.wrapper.send_command(f"!runas {command}")
+    
+        async def addnote(self, player, note):
+            return await self.wrapper.send_command(f"!addnote {player} {note}")
+    
+        async def list_players(self):
+            return await self.wrapper.send_command("!list")
+    
+        async def plugins(self):
+            return await self.wrapper.send_command("!plugins")
+    
+        async def reports(self):
+            return await self.wrapper.send_command("!reports")
+
+        async def offlinemessages(self):
+            return await self.wrapper.send_command("!offlinemessages")
+    
+        async def sayall(self, message):
+            return await self.wrapper.send_command(f"!sayall {message}")
+    
+        async def say(self, message):
+            return await self.wrapper.send_command(f"!say {message}")
+
+        async def rules(self):
+            return await self.wrapper.send_command("!rules")
+    
+        async def ping(self):
+            return await self.wrapper.send_command("!ping")
+    
+        async def setgravatar(self, email):
+            return await self.wrapper.send_command(f"!setgravatar {email}")
+    
+        async def help(self):
+            return await self.wrapper.send_command("!help")
+    
+        async def admins(self):
+            return await self.wrapper.send_command("!admins")
+    
+        async def privatemessage(self, player, message):
+            return await self.wrapper.send_command(f"!privatemessage {player} {message}")
+    
+        async def readmessage(self):
+            return await self.wrapper.send_command("!readmessage")
+    
+        async def report(self, player, reason): 
+            return await self.wrapper.send_command(f"!report {player} {reason}")
+        
+        #  Script Plugin  #
+        async def giveweapon(self, player, weapon):
+            return await self.wrapper.send_command(f"!giveweapon {player} {weapon}")
+    
+        async def takeweapons(self, player):
+            return await self.wrapper.send_command(f"!takeweapons {player}")
+    
+        async def lockcontrols(self, player):
+            return await self.wrapper.send_command(f"!lockcontrols {player}")
+
+        async def noclip(self):
+            return await self.wrapper.send_command("!noclip")
+    
+        async def alert(self, player, message):
+            return await self.wrapper.send_command(f"!alert {player} {message}")
+    
+        async def gotoplayer(self, player):
+            return await self.wrapper.send_command(f"!gotoplayer {player}")
+    
+        async def playertome(self, player):
+            return await self.wrapper.send_command(f"!playertome {player}")
+    
+        async def goto(self, x, y ,z):
+            return await self.wrapper.send_command(f"!goto {x} {y} {z}")
+    
+        async def kill(self, player):
+            return await self.wrapper.send_command(f"!kill {player}")
+
+        async def setspectator(self, player):
+            return await self.wrapper.send_command(f"!setspectator {player}")
+    
+        async def whitelistvpn(self, player):
+            return await self.wrapper.send_command(f"!whitelistvpn {player}")
+
+        async def disallowvpn(self, player):
+            return await self.wrapper.send_command(f"!disallowvpn {player}")
+    
+        async def bansubnet(self, subnet):
+            return await self.wrapper.send_command(f"!bansubnet {subnet}")
+    
+        async def unbansubnet(self, subnet):
+            return await self.wrapper.send_command(f"!unbansubnet {subnet}")
+    
+        async def switchteam(self, player):
+            return await self.wrapper.send_command(f"!switchteam {player}")
+        
+        #      Login      #
+        async def login(self, password):
+            return await self.wrapper.send_command(f"!login {password}")
+
+        #       Mute      #
+        async def mute(self, player, reason):
+            return await self.wrapper.send_command(f"!mute {player} {reason}")
+    
+        async def muteinfo(self, player):
+            return await self.wrapper.send_command(f"!muteinfo {player}")
+    
+        async def tempmute(self, player, duration, reason):
+            return await self.wrapper.send_command(f"!tempmute {player} {duration} {reason}")
+    
+        async def unmute(self, player, reason):
+            return await self.wrapper.send_command(f"!unmute {player} {reason}")
+        
+        #  Simple Status  #
+        async def mostkills(self):
+            return await self.wrapper.send_command("!mostkills")
+    
+        async def mostplayed(self):
+            return await self.wrapper.send_command("!mostplayed")
+    
+        async def rxs(self):
+            return await self.wrapper.send_command("!rxs")
+
+        async def topstats(self):
+            return await self.wrapper.send_command("!topstats")
+    
+        async def stats(self, player=None):
+            if player is None:
+                return await self.wrapper.send_command("!x")
+            else:
+                return await self.wrapper.send_command(f"!x {player}")
+    
+    async def get_logs(self):
+        temp_dir = tempfile.gettempdir()
+        logs = [file for file in os.listdir(temp_dir) if file.endswith(".log")]
+
+        if not logs:
+            logging.exception("No log files found")
+            return "No log files found"
+        
+        files = sorted(logs, key=lambda file: os.path.getctime(os.path.join(temp_dir, file)), reverse=True)
+        path = os.path.join(temp_dir, files[0])
+
+        try:
+            async with aiofiles.open(path, 'r') as f:
+                contents = await f.read()
+                return contents
+        except Exception as e:
+            logging.error(f"Error reading log file: {e}")
+            return f"Error reading log file: {e}"
