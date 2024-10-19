4c79e17f96858b9c6b1ddff5cd7a50cc63d29dd8
Yalla <132653472+Yallamaztar@users.noreply.github.com>
2024-10-19 13:36:32 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     author="budiworld",
     author_email="budi.world@yahoo.com",
     description="A Python wrapper for the IW4M-Admin API",
-    long_description=open('readme.md', encoding='utf-8').read(),
+    long_description=open('README.md', encoding='utf-8').read(),
     long_description_content_type='text/markdown',
     url="https://github.com/Yallamaztar/iw4m", 
     packages=find_packages(),
